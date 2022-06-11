ff015afd46f7372a3b343e5f1226cf53ed17f6a9
Kalindan <dikey@email.cz>
2022-06-12 00:26:11 +0200

    add endpoints

--- README.md
+++ README.md
@@ -11,19 +11,23 @@ The goal of this project is to create simple REST API service mimicing simple ba
 - Store persistent data in database
 - Authentication of customers
 - Validation of limits
+- Generate 10 digit account number
 ## Used technologies:
 - Python
 - FastAPI
+- SQLModel
+- PostgreSQL
 
 ## Bank system API endpoints
 |URL                                      | HTTP method |
 |-----------------------------------------|-------------|
 |http://127.0.0.1:8000/register-customer  |POST         |
 |http://127.0.0.1:8000/create-account     |POST         |
+|http://127.0.0.1:8000/set-limits         |POST         |
+|http://127.0.0.1:8000/get-balance        |GET          |
+|http://127.0.0.1:8000/get-limits         |GET          |
+|http://127.0.0.1:8000/transfer-money     |PUT          |
 |http://127.0.0.1:8000/withdraw-money     |PUT          |
 |http://127.0.0.1:8000/deposit-money      |PUT          |
-|http://127.0.0.1:8000/check-balance      |GET          |
-|http://127.0.0.1:8000/transfer-money     |PUT          |
-|http://127.0.0.1:8000/set-limits         |POST         |
 |http://127.0.0.1:8000/delete-account     |DELETE       |
 |http://127.0.0.1:8000/delete-customer    |DELETE       |
--- main.py
+++ main.py
@@ -1,20 +1,66 @@
-from fastapi import FastAPI, Query
-from pydantic import BaseModel
-import uvicorn
+from typing import List
+from fastapi import FastAPI, Body
+from pydantic import BaseModel, Field
+import uvicorn  #type:ignore
 
 app = FastAPI()
 
-class Account(BaseModel):
+class Customer(BaseModel):
     email:str
-    password:str = Query(min_length=10)
+    password:str = Field(min_length=10)
+    
+class Account(BaseModel):
+    number:int
+    pin_code:int
+    balance:float
 
-@app.get("/")
-def index():
-    return "Hello World"
+class Limits(BaseModel):
+    daily_limit:float
+    max_limit:float
+    
+
+customers : List[Customer] = []
+
+@app.post("/register-customer")
+def register_customer(new_customer: Customer):
+    customers.append(new_customer)
+    return "Customer succesfully registered"
 
 @app.post("/create-account")
-def create_account(new_account: NewAccount):
-    return f"Your email is {new_account.email} and your password is {new_account.password}"
+def create_account(new_account: Account, customer: Customer):
+    pass
+
+@app.post("/set-limits")
+def set_limits(account: Account, limits: Limits):
+    pass
+
+@app.get("/get-balance")
+def get_balance(account: Account):
+    pass
+
+@app.get("/get-limits")
+def get_limits(account: Account, limits: Limits):
+    pass
+
+@app.put("/transfer-money")
+def transfer_money(account_from: Account, account_to: Account, amount:int= Body()):
+    pass
+
+@app.put("/withdraw-money")
+def withdraw_money(account: Account, amount:int= Body()):
+    pass
+
+@app.put("/deposit-money")
+def deposit_money(account: Account, amount:int= Body()):
+    pass
+
+@app.delete("/delete-account")
+def delete_account(account: Account):
+    pass
+
+@app.delete("/delete-customer")
+def delete_customer(customer: Customer):
+    pass
 
 if __name__ == "__main__":
     uvicorn.run("main:app",reload=True)
