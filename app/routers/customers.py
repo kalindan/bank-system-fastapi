a7ae02fff2d74819957c936095d23954344c25ea
Daniel <dikey@email.cz>
2022-06-30 00:32:02 +0200

    formatting

--- app/routers/customers.py
+++ app/routers/customers.py
@@ -12,17 +12,17 @@ def register_customer(customer_write: CustomerWrite, session: Session = Depends(
                           .hash_password(customer_write.password)\
                           .db_create(session=session)
     customer_read = CustomerRead.from_orm(customer_db)
-    return JSONResponse(status_code=status.HTTP_201_CREATED,content=
-                        {"message": f"Customer {customer_read.id} successfully created",
-                         "customer_info":customer_read.dict()})
+    return JSONResponse(status_code=status.HTTP_201_CREATED,
+                        content={"message": f"Customer {customer_read.id} successfully created",
+                                 "customer_info":customer_read.dict()})
 
 
 @router.get("/{customer_id}")
 def customer_info(customer_id: int, session: Session = Depends(get_session)):
     customer = Customer(id=customer_id).db_read(session=session)
-    return JSONResponse(status_code=status.HTTP_200_OK,content=
-                        {"message": f"Customer {customer_id} successfully loaded",
-                         "customer_info":customer.dict()})
+    return JSONResponse(status_code=status.HTTP_200_OK,
+                        content={"message": f"Customer {customer_id} successfully loaded",
+                                 "customer_info":customer.dict()})
 
 
 @router.post("/{customer_id}", response_model=CustomerRead)
@@ -33,5 +33,5 @@ def login_customer(customer_id: int, session: Session = Depends(get_session)):
 @router.delete("/{customer_id}")
 def delete_customer(customer_id: int, session: Session = Depends(get_session)):
     Customer(id=customer_id).db_delete(session=session)
-    return JSONResponse(status_code=status.HTTP_200_OK,content=
-                        {"message": f"Customer {customer_id} successfully deleted"})
+    return JSONResponse(status_code=status.HTTP_200_OK,
+                        content={"message": f"Customer {customer_id} successfully deleted"})
