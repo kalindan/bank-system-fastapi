1ab1f79c3175a8adefc329701c196ad8923b66fc
Kalindan <dikey@email.cz>
2022-07-01 20:21:18 +0200

    typo

--- app/auth/oauth2.py
+++ app/auth/oauth2.py
@@ -32,7 +32,7 @@ def get_current_customer(
         if customer is None:
             raise HTTPException(
                 status_code=status.HTTP_404_NOT_FOUND,
-                detail="User not found",
+                detail="Customer not found",
             )
     except JWTError:
         raise HTTPException(
