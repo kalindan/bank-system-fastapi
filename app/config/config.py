f93c3fd52ebb88aed5fad5a20bbabf91d58957e3
Daniel <dikey@email.cz>
2022-07-13 09:31:46 +0200

    redo config

--- app/config/config.py
+++ app/config/config.py
@@ -1,14 +1,14 @@
 import os
-
+ 
 class Config:
-    secret_key: str = os.environ.get("SECRET_KEY", "")
-    db_url: str = os.environ.get("DB_URL", "")
 
-    @classmethod
-    def create(cls) -> "Config":
+    def __init__(self):
         if os.environ.get("ENV", "production") == "development":
-            cls.secret_key = "123456789"
-            cls.db_url = "sqlite:///database.db"
-        return Config()
+            self.secret_key = "123456789"
+            self.db_url = "sqlite:///database.db"
+        else:    
+            self.secret_key: str = os.environ.get("SECRET_KEY", "")
+            self.db_url: str = os.environ.get("DB_URL", "")
+        
 
-config = Config.create()
+config = Config()
